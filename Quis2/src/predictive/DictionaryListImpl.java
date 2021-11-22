ee79058e3196b995b88eb44ffc546153480f6bf7
Okyan Awang Ramadhana <okyanawang@gmail.com>
2021-11-22 20:39:20 +0700

    In progress : Solving second Main Point

---
+++ Quis2/src/predictive/DictionaryListImpl.java
@@ -0,0 +1,63 @@
+package predictive;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DictionaryListImpl {
+
+	public DictionaryListImpl() {
+		Set<String> stringSet = new HashSet<String>();
+		
+		// read file words.txt
+		try {
+			File words = new File("words.txt");
+			Scanner file = new Scanner(words);
+			
+			// if the file has next line
+			while (file.hasNextLine()) {
+				String data = file.nextLine();
+			}	
+				
+			// close the opened file
+			file.close();
+	    } catch (FileNotFoundException e) { // if the file not found
+	    	System.out.println("An error occurred.");
+	    	e.printStackTrace();
+	    }
+		
+		// return the result
+		return stringSet;
+	}
+	
+	public static String wordToSignature(String word) {
+		StringBuffer sb = new StringBuffer ("");
+		// incase there is upper-case letter
+		word = word.toLowerCase();
+		// for each letter paired with each number 
+		for (int i=0; i<word.length(); i++) {
+			if (word.charAt(i) >= 97 && word.charAt(i) <= 99) { // abc
+				sb.append("2");
+			}else if (word.charAt(i) >= 100 && word.charAt(i) <= 102) { // def
+				sb.append("3");
+			}else if (word.charAt(i) >= 103 && word.charAt(i) <= 105) { // ghi
+				sb.append("4");
+			}else if (word.charAt(i) >= 106 && word.charAt(i) <= 108) { // jkl
+				sb.append("5");
+			}else if (word.charAt(i) >= 109 && word.charAt(i) <= 111) { // mno
+				sb.append("6");
+			}else if (word.charAt(i) >= 112 && word.charAt(i) <= 115) { // pqrs
+				sb.append("7");
+			}else if (word.charAt(i) >= 116 && word.charAt(i) <= 118) { // tuv
+				sb.append("8");
+			}else if (word.charAt(i) >= 119 && word.charAt(i) <= 122) { // wxyz
+				sb.append("9");
+			}else {
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
+	}
+
+}
---
+++ Quis2/src/predictive/WordSig.java
@@ -0,0 +1,14 @@
+package predictive;
+
+public class WordSig implements Comparable <WordSig>{
+	private String words;
+	private String signature;
+	public WordSig (String Tarte, String Maha) {
+		this.words = Tarte;
+		this.signature = Maha;
+	}
+	public int compareTo(WordSig ws) {
+		
+	}
+	..
+}
