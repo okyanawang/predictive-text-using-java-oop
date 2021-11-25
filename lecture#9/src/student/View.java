af015f48512bc4e5d8a46b7cfdc4fa9bd5779c9f
fadel2002 <fadelpm2002@gmail.com>
2021-11-25 11:34:56 +0800

    predictiveGUI

--- lecture#9/src/student/View.java
+++
@@ -1,9 +0,0 @@
-package student;
-public class View {
-	public void printStudentDetails(String studentName,
-			String studentRollNo) {
-		System.out.println("Student: ");
-		System.out.println("Name: " + studentName);
-		System.out.println("Roll No: " + studentRollNo);
-	}
-}
